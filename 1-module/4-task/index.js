c1c321b91e97ef273fd38bb6cde82d9c5782684c
timan <timanagovicin@yandex.ru>
2023-11-19 00:02:11 +0300

    Fix mistakes with if

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,4 +1,3 @@
 function checkSpam(str) {
-  if (str.toUpperCase().includes('1XBET') || str.toUpperCase().includes('XXX')) return true;
-  else return false;
+  return str.toUpperCase().includes('1XBET') || str.toUpperCase().includes('XXX');
 }
