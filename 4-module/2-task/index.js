42f077ac1e5469887de600a0440708693ffcf654
timan <timanagovicin@yandex.ru>
2023-12-08 16:55:26 +0300

    4 lesson tasks

--- 4-module/2-task/index.js
+++ 4-module/2-task/index.js
@@ -1,3 +1,11 @@
 function makeDiagonalRed(table) {
-  // ваш код...
+    let rows = table.getElementsByTagName("tr");
+    for (let i = 0; i < rows.length; i++) {
+        let cells = rows[i].getElementsByTagName("td");
+        for (let j = 0; j < cells.length; j++) {
+            if (i == j) {
+                cells[i].style.backgroundColor = "red";
+            }
+        }
+    };
 }
